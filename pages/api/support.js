662f46a042e66b4fb0e6557e4521305dd3450bcc
unknown <1255475+iismaell@users.noreply.github.com>
2022-12-21 12:26:17 +0800

    avia openai widget

---
+++ pages/api/support.js
@@ -0,0 +1,25 @@
+
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+    const answer =await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: req.body.question,
+        temperature: 0.27,
+        max_tokens: 4000,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+
+    console.log(answer)
+
+    res.status(200).json({ result: answer.data.choices[0].text });
+}
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,82 @@
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
+import { BsLightningChargeFill } from 'react-icons/bs';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+    const [questionInput, setQuestionInput] = useState("");
+    const [result, setResult] = useState();
+    const [active, setActive] = useState(false);
+    const [copied, setCopied] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-  async function onSubmit(event) {
-    event.preventDefault();
-    const response = await fetch("/api/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ animal: animalInput }),
-    });
-    const data = await response.json();
-    setResult(data.result);
-    setAnimalInput("");
-  }
+    async function onSubmit(event) {
+        event.preventDefault();
+        setCopied(false);
+        setLoading(true);
+        setResult("");
+        const response = await fetch("/api/support", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ question: questionInput }),
+        });
+        const data = await response.json();
+        setLoading(false);
+        setResult(data.result);
+        setQuestionInput("");
+    }
 
-  return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+    return (
+        <div className={styles.wrapper}>
+            <Head>
+                <title>OpenAI Quickstart</title>
+            </Head>
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-    </div>
-  );
+            <main className={styles.main}>
+                {active &&
+                    <div className={styles.support}>
+                        <h3>Support for Support</h3>
+                        <form onSubmit={onSubmit}>
+                            <textarea
+                                type="text"
+                                name="question"
+                                placeholder="Ask me anything"
+                                value={questionInput}
+                                onChange={(e) => setQuestionInput(e.target.value)}
+                                rows="5"
+                                cols="60"
+                                className={styles.question}
+                            ></textarea>
+
+                            <input type="submit" value="Submit Question" />
+                        </form>
+                        {loading && <span className={styles.loader}></span>}
+                        {result &&
+                            <>
+                                <div className={styles.result}>
+                                    <h4>Possible answer:</h4>
+                                    <p>{result}</p>
+                                    <div className={styles.copy}>
+                                        <CopyToClipboard
+                                            text={result.trim()}
+                                            onCopy={() => setCopied(true)}>
+                                            <span>Copy to clipboard</span>
+                                        </CopyToClipboard>
+                                    </div>
+                                    {copied && <span>To the forum!</span>}
+                                </div>
+                            </>
+                        }
+                    </div>}
+
+                <div className={styles.launcher} onClick={() => setActive(!active)} >
+                    <BsLightningChargeFill className={styles.lightning} />
+                </div>
+            </main>
+        </div>
+    );
 }
+
